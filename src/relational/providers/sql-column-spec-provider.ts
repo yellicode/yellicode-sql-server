ced5004ca980aae7a4a59bf275821cf033bfdc03
Maurice de Laat <info@mauricedelaat.nl>
2020-05-07 16:43:32 +0200

    Extended public API.

--- src/relational/providers/sql-column-spec-provider.ts
+++ src/relational/providers/sql-column-spec-provider.ts
@@ -4,10 +4,10 @@ export interface SqlColumnSpecProvider {
      /**
      * The maximum size, in bytes, of the data within the column. Set to -1 to specifify a maximum length.
      */
-    getLength(sqlTypeName: string, property?: elements.Property): number | null;   
+    getLength(sqlTypeName: string, property?: elements.Property): number | null;
     getPrecision(sqlTypeName: string, property?: elements.Property): number | null;
     getScale(sqlTypeName: string, property?: elements.Property): number | null;
-    isRelationship(property: elements.Property): boolean;    
+    isRelationship(property: elements.Property): boolean;
 }
 
 export class DefaultSqlColumnSpecProvider implements SqlColumnSpecProvider {
@@ -17,19 +17,19 @@ export class DefaultSqlColumnSpecProvider implements SqlColumnSpecProvider {
 
     protected  /*virtual */ requiresLength(sqlTypeName: string, property?: elements.Property): boolean {
         return false;
-        
+
     }
 
     public /*virtual */ getPrecision(sqlTypeName: string, property?: elements.Property): number | null {
         return null;
-     
+
     }
 
     public /*virtual */ getScale(sqlTypeName: string, property?: elements.Property): number | null {
-        return null;      
+        return null;
     }
 
     public /*virtual */ isRelationship(property: elements.Property): boolean {
-        return !elements.isDataType(property.type);
+        return property.type != null && !elements.isDataType(property.type);
     }
 }
